640495ccd785842ef8713cd6d4492dfd0a067adf
OlgaKosm <romanykolya@tut.by>
2023-02-14 16:34:51 +0200

    feat: add solution for task "Reverse Int"

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
+    let res;
+    let positiveNum = Math.abs(n);
+
+    res = Number(String(positiveNum).split('').reverse().join(''));
+    return res;
   
 }
